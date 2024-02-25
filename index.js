548fcd6bd7c55c61ec4e6de8704702d6df2994c3
baldwisj <baldwisj@gmail.com>
2024-02-25 14:14:48 -0500

    Added test to readme and commented on code

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+//Install the packages we need to run this program
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+//Import the shape classes that we defined in the shapes.js file
 const { Square, Triangle, Circle } = require('./lib/shapes.js');
 
+//The following function initiates various code to create the sting that is the SVG
 const buildLogoSVG = (file, choices) => {
+    //The following adds code to the logo variable which we will use to create the SVG
     let logo;
     logo = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
     logo += "<g>";
     logo += `${choices.shape}`;
 
-
+    //This dertermines which shape was selected by the user
     let logoShape;
     if (choices.shape === 'Square') {
         logoShape = new Square();
@@ -19,20 +23,24 @@ const buildLogoSVG = (file, choices) => {
         logoShape = new Circle();
     }
 
+    //The following adds the color into the shape SVG string and then adds it to the sections created above
     logoShape.colorChoice(choices.logoColor);
     const logoSVG = logoShape.render();
 
+    //the final peices of the SVG are added
     logo += logoSVG;
     logo += `<text x="150" y="130" text-anchor="middle" font-size="60" fill="${choices.textColor}">${choices.text}</text>`;
     logo += "</g>";
     logo += '</svg>'
 
+    //A new file is created where the logo SVG is pushed to
     fs.writeFile(file, logo, (error) => {
         error ? console.log('Unable to create file') : console.log('logo.svg has been created');
     });
     
 };
 
+//The prompt function is created and the prompt is initiated
 const clientPrompt = () => {
     inquirer.prompt([
         {
@@ -40,6 +48,7 @@ const clientPrompt = () => {
             message: "Enter up to three characters for logo.",
             name: "text",
             validate: function (input) {
+                //Added stipulation of up to 3 characters
                 if (isNaN(input) && input.length > 1 && input.length < 3) {
                     return 'Please enter one, two, or three characters.';
                 }
@@ -51,6 +60,7 @@ const clientPrompt = () => {
             message: "Please enter color key or hex value for text color.",
             name: "textColor",
             validate: function (input) {
+                //regex added to determine if an entered value is a valid hex value or color keyword
                 const hexColorRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
                 if (!input.match(hexColorRegex) && !isColorKeyword(input)) {
                     return 'Please enter a valid color keyword or hex value.';
@@ -69,6 +79,7 @@ const clientPrompt = () => {
             message: "Please enter color key or hex value for logo color.",
             name: "logoColor",
             validate: function (input) {
+                //regex added to determine if an entered value is a valid hex value or color keyword
                 const hexColorRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
                 if (!input.match(hexColorRegex) && !isColorKeyword(input)) {
                     return 'Please enter a valid color keyword or hex value.';
@@ -78,11 +89,12 @@ const clientPrompt = () => {
 
         },
     ])
+    //Once the prompt is answered the then function will call the function the peice together the SVG with the given choices
         .then((choices) => {
             buildLogoSVG("logo.svg", choices);
         });
 }
-
+//the following creates a list of the possible color keywords
 const isColorKeyword = (input) => {
     const colorKeywords = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 'lime', 'maroon', 'navy', 'olive', 'purple', 'red', 'silver', 'teal', 'white', 'yellow'];
     return colorKeywords.includes(input.toLowerCase());
