b9e12c7e526788b008564a42f968a46379453b0e
baldwisj <baldwisj@gmail.com>
2024-02-25 13:17:33 -0500

    Corrected errors and finished file creation functionality

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { Square, Triangle, Circle } = require('./lib/shapes.js');
 const buildLogoSVG = (file, choices) => {
     let logo;
     logo = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-
+    logo += "<g>";
     logo += `${choices.shape}`;
 
 
@@ -15,7 +15,7 @@ const buildLogoSVG = (file, choices) => {
         logoShape = new Square();
     } else if (choices.shape === 'Triangle') {
         logoShape = new Triangle();
-    } else (choices.shape === 'Circle'){
+    } else if (choices.shape === 'Circle') {
         logoShape = new Circle();
     }
 
@@ -23,19 +23,25 @@ const buildLogoSVG = (file, choices) => {
     const logoSVG = logoShape.render();
 
     logo += logoSVG;
+    logo += `<text x="150" y="130" text-anchor="middle" font-size="60" fill="${choices.textColor}">${choices.text}</text>`;
+    logo += "</g>";
     logo += '</svg>'
 
-    return logo
+    fs.writeFile(file, logo, (error) => {
+        error ? console.log('Unable to create file') : console.log('logo.svg has been created');
+    });
+    
 };
+
 const clientPrompt = () => {
     inquirer.prompt([
         {
-            type: "imput",
+            type: "input",
             message: "Enter up to three characters for logo.",
             name: "text",
             validate: function (input) {
-                if (isNaN(input || input.length > 3)) {
-                    return 'Please enter one, teo, or three characters.';
+                if (isNaN(input) && input.length > 1 && input.length < 3) {
+                    return 'Please enter one, two, or three characters.';
                 }
                 return true;
             }
@@ -43,24 +49,43 @@ const clientPrompt = () => {
         {
             type: "input",
             message: "Please enter color key or hex value for text color.",
-            name: "text Color",
+            name: "textColor",
+            validate: function (input) {
+                const hexColorRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+                if (!input.match(hexColorRegex) && !isColorKeyword(input)) {
+                    return 'Please enter a valid color keyword or hex value.';
+                }
+                return true;
+            }
         },
         {
             type: "list",
             message: "Please choose your desired logo shape.",
-            choices: ["Square, Triangle, Circle"],
+            choices: ["Square", "Triangle", "Circle"],
             name: "shape",
         },
         {
             type: "input",
             message: "Please enter color key or hex value for logo color.",
             name: "logoColor",
+            validate: function (input) {
+                const hexColorRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+                if (!input.match(hexColorRegex) && !isColorKeyword(input)) {
+                    return 'Please enter a valid color keyword or hex value.';
+                }
+                return true;
+            }
 
         },
     ])
         .then((choices) => {
-    writeTofile("logo.svg", choices);
-});
+            buildLogoSVG("logo.svg", choices);
+        });
+}
+
+const isColorKeyword = (input) => {
+    const colorKeywords = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 'lime', 'maroon', 'navy', 'olive', 'purple', 'red', 'silver', 'teal', 'white', 'yellow'];
+    return colorKeywords.includes(input.toLowerCase());
 }
 
 clientPrompt();
